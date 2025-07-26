5c7251a8372df2c83f26d54c1e7ee8d245056f41
Alexander <algurev0220@gmail.com>
2025-07-27 00:49:54 +0300

    bootstrap color

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -27,18 +27,19 @@ function Login({setLogin, setPassword}: LoginProps): ReactNode {
     }
 
     return (
-        <div className="card">
-            <div className="row">
+        <div className="card bg-success-subtle">
+            <div className="row mb-3">
                 <label htmlFor="loginInput" className="col-4">Login:</label>
                 <input type="text" id="loginInput" name="loginInput" value={newLogin}  className="float-start col-8"
                        onChange={(e) => newLogin = e.target.value}/>
             </div>
-            <div>
-                <label htmlFor="password">Password:</label>
-                <input type="password" id="password" name="password" value={newPassword}
+            <div className="row mb-3">
+                <label htmlFor="password" className="col-4">Password:</label>
+                <input type="password" id="password" name="password" value={newPassword} className="float-start col-8"
                        onChange={(e) => newPassword = e.target.value}/>
             </div>
-            <button onClick={() => logIn()}>Log in</button>
+            <button className="mb-3" onClick={() => logIn()}>Log in</button>
+            <button onClick={() => setRegistration(true)}>Register</button>
             {registration ? "Reg" : ""}
         </div>
     )
