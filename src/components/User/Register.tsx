b1ec41c92701497784ddf613765c196063359e6b
Alexander <algurev0220@gmail.com>
2025-11-19 04:45:11 +0200

    registration-1 working

--- src/components/User/Register.tsx
+++ src/components/User/Register.tsx
@@ -1,5 +1,5 @@
 import {type ReactNode, useState} from "react";
-import type {RegisterProps} from "../../utils/Interfaces.ts"
+import type {RegisterProps, UserDto} from "../../utils/Interfaces.ts"
 import '../Common/FarmStyles.css'
 import './Register.css'
 import {SERVER_URL} from "../../utils/Urls.ts";
